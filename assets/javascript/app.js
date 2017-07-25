f50ad6a8cbdb64bdafa807f7ed4886d80b090e2c
Corina Mun <corinamun@corinas-air.gateway.pace.com>
2017-07-24 22:16:29 -0700

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,69 @@
+//Stopwatch variables
+
+var clockRunning = false;
+var stopwatch = {
+	time: 
+}
+
+//Variables
+var numberOfQuestions=7 
+var correctAnswers= [0,1,2,3,4,5,6,7]
+correctchoices[1]='C' 
+correctchoices[2]='D' 
+correctchoices[3]='B'
+correctchoices[4]='B'
+correctchoices[5]='B'
+correctchoices[6]='D'
+correctchoices[7]='C'
+
+function gradeAnswers(){
+var incorrect=null
+for (q=1;q<=totalquestions;q++){
+	var thequestion=eval("document.myquiz.question"+q)
+	for (c=0;c<thequestion.length;c++){
+		if (thequestion[c].checked==true)
+		actualchoices[q]=thequestion[c].value
+		}
+		
+	if (actualchoices[q]!=correctchoices[q]){ //process an incorrect choice
+		if (incorrect==null)
+		incorrect=q
+		else
+		incorrect+="/"+q
+		}
+	}
+
+if (incorrect==null)
+incorrect="a/b"
+document.cookie='q='+incorrect
+if (document.cookie=='')
+alert("Your browser does not accept cookies. Please adjust your browser settings.")
+else
+window.location="results.htm"
+}
+
+
+function showsolution(){
+var win2=window.open("","win2","width=200,height=350, scrollbars")
+win2.focus()
+win2.document.open()
+win2.document.write('<title>Solution</title>')
+win2.document.write('<body bgcolor="#FFFFFF">')
+win2.document.write('<center><h3>Solution to Quiz</h3></center>')
+win2.document.write('<center><font face="Arial">')
+for (i=1;i<=totalquestions;i++){
+for (temp=0;temp<incorrect.length;temp++){
+if (i==incorrect[temp])
+wrong=1
+}
+if (wrong==1){
+win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
+wrong=0
+}
+else
+win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
+}
+win2.document.write('</center></font>')
+win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5><p align='center'><small><a href='http://www.javascriptkit.com' target='_new'>JavaScript Kit quiz script</a></small>")
+win2.document.close()
+}
